1a5e10d87204e065b6b010aad6a887f9caaf1e76
SoullessSoldier <48070769+SoullessSoldier@users.noreply.github.com>
2021-12-09 23:09:43 +0200

    add Chapter 6 - meet the components

---
+++ 6-components/10-single-file-component.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="hello">
+        {{msg}}
+    </div>
+</template>
+<script>
+export default {
+    name: 'hello',
+    data(){
+        return {
+            msg: 'Hello world!'
+            }
+    }
+}
+</script>
+<style scoped>
+    .hello{
+        color: red;
+    }
+</style>
